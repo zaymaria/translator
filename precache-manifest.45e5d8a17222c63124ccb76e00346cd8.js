85008b6c3c92c28d4786db3d6c6c2124cd391d6d
zaymaria <zaymaria87@gmail.com>
2023-11-16 17:02:45 +0100

    First

---
+++ precache-manifest.45e5d8a17222c63124ccb76e00346cd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf56a7329bbbf70afc2666e56f52015e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa2e255cc9d77396f649",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "8019ba14a45a43c4cada",
+    "url": "/static/js/2.c738aac6.chunk.js"
+  },
+  {
+    "revision": "fa2e255cc9d77396f649",
+    "url": "/static/js/main.5debd23d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
